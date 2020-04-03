cbc1f20d14e8bc16ee7c70bca11120221c6e0a8f
Jason McHugh <mchughj@gmail.com>
2020-04-03 12:44:42 -0700

    Multiple packets all working
    This is a bare-bones v0.1!  It supports multiple verbs in the status screen and also correctly sends large packets of data which are broken up appropriately.  Acknowledgments for the data all work correctly too.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # iBoardBot
-My take on the iBoardBot software
+
+I bought an iBoardBot many years ago and have spent some time upgrading the
+physical installation but never iterated on the software.  This repo
+encapsulates my efforts to improve the general eco-system.
+
+For details on iBoardBot see
+their [https://www.jjrobots.com/iboardbot-assembly-instructions/](assembly instructions).
+The build is a lot of fun and I would highly recommend getting one.
+
+
--- code/server/server.py
+++ code/server/server.py
@@ -1,14 +1,14 @@
 
 import time
 import struct
-import queue
 import argparse
 
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
 
 from functools import partial
 import logging
 import urllib.parse
+import threading
 
 logging.basicConfig(level=logging.DEBUG, format='(%(threadName)-10s) %(message)s')
 
@@ -17,25 +17,170 @@ parser.add_argument('--port', type=int, help='Port to listen on', default=8080)
 parser.add_argument('--data', type=str, help='Location to save persistent data', default='./data')
 config = parser.parse_args()
 
-is_first = True
-
 DEVICE_URL_PREFIX = "/ibb-device/"
+CLIENT_ID = "ID_IWBB"
+
+
+def moveToPos(x, y):
+  full       = (x << 12) + y
+
+  msbByte    = (full >> 16 ) & 0xFF
+  middleByte = (full >> 8 )  & 0xFF
+  lsbByte    = (full)        & 0xFF
+
+  return struct.pack("BBB", msbByte, middleByte, lsbByte)
+
+
+def block(blockNumber):
+  full       = (4009 << 12) + blockNumber            #   4009 0001   [FA9001]     # Block number 0001 (example)
+
+  msbByte    = (full >> 16 ) & 0xFF
+  middleByte = (full >> 8 )  & 0xFF
+  lsbByte    = (full)        & 0xFF
+
+  return struct.pack("BBB", msbByte, middleByte, lsbByte)
+
+
+def mockDrawData(size = 0):
+  if size == 0:
+    result  = struct.pack("BBB", 0xFA, 0x30, 0x00 )   #   4003 0000   [FA3000]    # pen lift
+    result += struct.pack("BBB", 0x00, 0x00, 0x00 )   #   0000 0000   [000000]    # Move to X = 0, Y = 0
+    result += struct.pack("BBB", 0x3E, 0x83, 0xE8 )   #   1000 1000   [3E83E8]    # Move to X = 100mm, Y = 100mm (1000 of 0.1mm)
+    result += struct.pack("BBB", 0xFA, 0x40, 0x00 )   #   4004 0000   [FA4000]    # pen down (draw)
+    result += struct.pack("BBB", 0x5D, 0xC3, 0xE8 )   #   1500 1000   [5DC3E8]    # Move to X = 150mm, Y = 100mm
+    result += struct.pack("BBB", 0xFA, 0x30, 0x00 )   #   4003 0000   [FA3000]    # Pen lift
+  else:
+    result  = struct.pack("BBB", 0xFA, 0x30, 0x00 )   #   4003 0000   [FA3000]    # pen lift
+    result += struct.pack("BBB", 0x00, 0x00, 0x00 )   #   0000 0000   [000000]    # Move to X = 0, Y = 0
+    for x in range(size * 2):
+      for y in range(size * 5):
+        result += moveToPos(1000 + (x*100), 1000 - (y*50))   #  - Move - 
+        result += struct.pack("BBB", 0xFA, 0x40, 0x00 )      #   4004 0000   [FA4000]    # pen down (draw)
+        result += moveToPos(1075 + (x*100), 1000 - (y*50))   #  - Move - 
+        result += struct.pack("BBB", 0xFA, 0x30, 0x00 )      #   4003 0000   [FA3000]    # Pen lift
+
+  logging.info("mockData - done; size: %d, resultSize: %d", size, len(result))
+  return result
+
+class NoWorkException(Exception):
+  pass
 
 
 class Client(object):
+
+  HEADER_COMMANDS_FOR_FIRST_PACKET = 3
+  SIZE_OF_COMMAND = 3
+  HEADER_COMMANDS_FOR_SUBSEQUENT_PACKET = 2
+
   def __init__(self, clientId):
     self.numberOfAccesses = 1
     self.clientId = clientId
     self.createdMs = round(time.time() * 1000)
     self.lastAccessMs = round(time.time() * 1000)
-    self.queue = queue.Queue()
+    self.lock = threading.Lock()
+    self.condition = threading.Condition()
+    self.queue = []
+    self.nextBlockNumber = 0
 
   def recordAccess(self):
     self.numberOfAccesses += 1
     self.lastAccessMs = round(time.time() * 1000)
 
-  def getQueue(self):
-    return self.queue
+  def popQueueUntilBlockNumber(self, blockNumber):
+    logging.info("popQueueUntilBlockNumber - onEnter; blockNumber: %d", blockNumber)
+    numPopped = 0
+    with self.condition:
+      while len(self.queue):
+        logging.info("popQueueUntilBlockNumber - queue has more items; len: %d", len(self.queue))
+        (qBlockNumber, qItem) = self.queue[0]
+        logging.info("popQueueUntilBlockNumber - top of queue; qBlockNumber: %d", qBlockNumber)
+        if qBlockNumber <= blockNumber:
+          numPopped += 1
+          self.queue = self.queue[1:]
+        else:
+          break
+      logging.info("popQueueUntilBlockNumber - done with work; numPopped: %d, size of Queue remaining: %d", numPopped, len(self.queue))
+
+  def clearQueue(self):
+    with self.condition:
+      self.queue = []
+
+  def addNewDrawing(self, payload):
+    # The data can be arbitrary size and we need to break it up into sizes at
+    # most 768 - HEADER_SIZE bytes long at a maximum that can be transferred in
+    # a single chunk.
+
+    # First add the footer to the payload
+    data = self._addFooterToData(payload)
+
+    numBlocks = 0;
+    headerSize = Client.HEADER_COMMANDS_FOR_FIRST_PACKET * Client.SIZE_OF_COMMAND
+    isFirst = True
+
+    with self.condition:
+      while len(data) > 0:
+        numBlocks+=1
+        dataSize = len(data)
+        if dataSize + headerSize > 768:
+          dataSize = 768 - headerSize
+
+        self._addNewBlock(isFirst, data[:dataSize])
+        data = data[dataSize:]
+
+        headerSize = Client.HEADER_COMMANDS_FOR_SUBSEQUENT_PACKET * Client.SIZE_OF_COMMAND
+        isFirst = False
+      self.condition.notify()
+
+    logging.info("addNewDrawing - done; numBlocks: %d", numBlocks)
+
+  def _addHeaderToData(self, isFirst, blockNumber, payload):
+    if isFirst:
+      result  = block(4001) 
+      result += block(blockNumber) 
+      result += struct.pack("BBB", 0xFA, 0x1F, 0xA1 )           #   4001 4001   [FA1FA1]    # Start drawing (new draw)
+      result += payload
+      return result
+    else:
+      result  = block(4001) 
+      result += block(blockNumber)                              
+      result += payload
+      return result
+
+  def _addFooterToData(self, payload):
+    result  = payload 
+    result += struct.pack("BBB", 0x00, 0x00, 0x00 )             #   0000 0000   [000000]    # Move to 0,0
+    result += struct.pack("BBB", 0xFA, 0x20, 0x00 )             #   4002 0000   [FA2000]    # Stop drawing
+    return result
+
+  def _addNewBlock(self, isFirst, data):
+    if len(self.queue) == 0:
+      self.nextBlockNumber = 0
+
+    self.nextBlockNumber += 1
+    data = self._addHeaderToData(isFirst, self.nextBlockNumber, data)
+
+    logging.info("addNewBlock - enqueue; nextBlockNumber: %d, size: %d", self.nextBlockNumber, len(data))
+
+    entry = (self.nextBlockNumber, data)
+    self.queue.append(entry)
+    logging.info("addNewBlock - final results; queue: %s", self.queue)
+
+  def getNextBlock(self, timeoutSeconds):
+    logging.info("getNextBlock - on enter;")
+    with self.condition:
+      while len(self.queue) == 0:
+        result = self.condition.wait(timeoutSeconds)
+        if result == False:
+          logging.info("getNextResult - no work available; timeoutSeconds: %d", timeoutSeconds)
+          raise NoWorkException()
+
+      (blockNumber, data) = self.queue[0]
+      logging.info("getNextBlock - found a block; blockNumber: %d", blockNumber)
+      return (blockNumber, data)
+
+  def getQueueSize(self):
+    with self.condition:
+      return len(self.queue)
 
 
 class ClientManager(object):
@@ -48,6 +193,7 @@ class ClientManager(object):
   def getOrMakeClient(self, clientId):
     c = self.getClient(clientId)
     if c is None:
+      logging.info("getOrMakeClient - creating new client; clientId: %s", clientId);
       c = Client(clientId)
       self.clientDevices[clientId] = c
     return c
@@ -69,7 +215,7 @@ class MyHandler(BaseHTTPRequestHandler):
   def sendText(self, s):
     self.wfile.write(bytes(s,"utf-8"))
 
-  def showMainMenu(self):
+  def showMainMenu(self, message = None):
     logging.info("showMainMenu")
     self.send_response(200)
     self.send_header('Content-type', 'text/html')
@@ -85,41 +231,45 @@ class MyHandler(BaseHTTPRequestHandler):
     self.sendText("</head>")
     self.sendText("<h1>iBoardBot Server</h1>")
     self.sendText("<br><br>")
+    if message:
+      self.sendText("<font color=\"red\">")
+      self.sendText(message)
+      self.sendText("</font></hr></br>")
+
     self.sendText("Clients<br>")
     self.sendText("<table style=\"width:100%\">")
-    self.sendText("<tr><th>ID</th><th>Queue Size</th></tr>")
+    self.sendText("<tr><th>ID</th><th>Queue Size</th><th>Actions</th></tr>")
     clientIds = self.clientManager.getClientIds()
     for c in clientIds:
       self.sendText("<tr><td>")
       self.sendText(c)
       self.sendText("</td><td>")
-      self.sendText(self.clientManager.getClient(c).getQueue().qsize() )
+      self.sendText(str(self.clientManager.getClient(c).getQueueSize() ))
+      self.sendText("</td><td>")
+      urlEncodedArgs = urllib.parse.urlencode({CLIENT_ID:c, "size":0 })
+      self.sendText("[")
+      self.sendText("<a href=\"/addMockData?{args}\">Dummy Packet</a>".format(args=urlEncodedArgs))
+      self.sendText("|")
+      urlEncodedArgs = urllib.parse.urlencode({CLIENT_ID:c, "size":1 })
+      self.sendText("<a href=\"/addMockData?{args}\">Big Dummy Packet</a>".format(args=urlEncodedArgs))
+      self.sendText("|")
+      urlEncodedArgs = urllib.parse.urlencode({CLIENT_ID:c})
+      self.sendText("<a href=\"/clearQueue?{args}\">Clear Queue</a>".format(args=urlEncodedArgs))
+      self.sendText("]")
       self.sendText("</td>")
+      self.sendText("</tr>")
     self.sendText("</table>")
     self.sendText("</html>")
 
   def do_GET(self):
     logging.debug("do_GET - received a GET request; path: %s", self.path)
 
-    if self.path.startswith("http://"):
-      # Instead of just the path it appears that the path argument
-      # has the entire string for the URL in it.  This shouldn't happen and
-      # is likely a bug in the firmware for the iBoardBot
-      parsePath = urllib.parse.urlparse(self.path)
-      logging.debug("do_GET - transformed path; parsePath: %s", parsePath)
+    parsePath = urllib.parse.urlparse(self.path)
+    self.path = parsePath.path
+    self.args = urllib.parse.parse_qs(parsePath.query)
 
-      self.path = parsePath.path
-      self.args = urllib.parse.parse_qs(parsePath.query)
-
-      logging.debug("do_GET - pulled apart path and args; path: %s, args: %s",
-          self.path, self.args)
-    else:
-      queryArgsLocation = self.path.find("?")
-      if queryArgsLocation > 0:
-        self.args = urllib.parse.parse_qs(self.path[queryArgsLocation+1:])
-      else:
-        self.args = urllib.parse.parse_qs("")
-      logging.debug("do_GET - path left alone; args: %s", self.args)
+    logging.debug("do_GET - pulled apart path and args; path: %s, args: %s",
+        self.path, self.args)
 
     if self.isDeviceRequest():
       logging.debug("do_GET - this is a device request;")
@@ -131,10 +281,18 @@ class MyHandler(BaseHTTPRequestHandler):
   def isDeviceRequest(self):
     return self.path.startswith(DEVICE_URL_PREFIX)
 
+  def clearQueue(self, clientId):
+    c = self.clientManager.getClient(clientId)
+    c.clearQueue()
+
+  def addMockData(self, clientId, size):
+    c = self.clientManager.getClient(clientId)
+    c.addNewDrawing(mockDrawData(size))
+
   def handleDeviceRequest(self):
-    clientId = self.args["ID_IWBB"][0]
+    clientId = self.args[CLIENT_ID][0]
     if "NUM" in self.args:
-      ackBlockNumber = self.args["NUM"][0]
+      ackBlockNumber = int(self.args["NUM"][0])
     else:
       ackBlockNumber = None
 
@@ -145,8 +303,17 @@ class MyHandler(BaseHTTPRequestHandler):
       self.showMainMenu()
     elif self.path == "/favicon.ico":
       self.send_error(404)
+    elif self.path == "/clearQueue":
+      clientId = self.args[CLIENT_ID][0]
+      self.clearQueue(clientId)
+      self.showMainMenu("Queue cleared!")
+    elif self.path == "/addMockData":
+      clientId = self.args[CLIENT_ID][0]
+      size = int(self.args["size"][0])
+      self.addMockData(clientId, size)
+      self.showMainMenu("Mock data added!")
     elif self.path.startswith("/puttext?"):
-      clientId = self.args["ID_IWBB"][0]
+      clientId = self.args[CLIENT_ID][0]
       text = self.args["TEXT"][0]
 
       self.enqueueText(clientId, text)
@@ -156,21 +323,19 @@ class MyHandler(BaseHTTPRequestHandler):
       self.send_error(404)
 
   def getNextDeviceBlock(self, clientId, ackBlockNumber):
-    logging.info("Got a device request; clientId: %s, ackBlockNumber: %s",
+    logging.info("getNextDeviceBlock - Got a request; clientId: %s, ackBlockNumber: %s",
         clientId, ackBlockNumber)
     
     c = self.clientManager.getOrMakeClient(clientId)
     if ackBlockNumber:
-      c.ackBlockNumber(ackBlockNumber)
+      c.popQueueUntilBlockNumber(ackBlockNumber)
 
-    if is_first:
-      c.getQueue().push(mockResult)
-      is_first = False
-
-    if c.getQueue().empty():
+    try:
+      (blockNumber, data) = c.getNextBlock(timeoutSeconds=10)
+      self.sendDeviceResult(data)
+    except NoWorkException:
       self.sendDeviceEmptyResult()
-    else:
-      self.sendDeviceResult(c.getQueue().get())
+
 
   def sendDeviceEmptyResult(self):
     logging.info("sendingDeviceEmptyResult; ")
@@ -181,10 +346,10 @@ class MyHandler(BaseHTTPRequestHandler):
     self.send_header('Content-type', 'text/html')
     self.send_header("Content-Length", "2")
     self.end_headers()
-    self.wfile.write(bytes("OK","utf-8"))
+    self.sendText("OK")
 
   def sendDeviceResult(self, data):
-    logging.info("sendDeviceResult - data;")
+    logging.info("sendDeviceResult - onEnter;")
 
     self.send_response(200)
     self.send_header('Content-type', 'text/html')
@@ -192,20 +357,6 @@ class MyHandler(BaseHTTPRequestHandler):
     self.end_headers()
     self.wfile.write(data)
 
-  def mockResult(self):
-    result  = struct.pack("BBB", 0xFA, 0x9f, 0xA1 )   #   4009 4001   [FA9FA1]    # Start
-    result += struct.pack("BBB", 0xFA, 0x90, 0x01 )   #   4009 0001   [FA9001]    # Block number (this number is calculated by the server)
-    result += struct.pack("BBB", 0xFA, 0x1F, 0xA1 )   #   4001 4001   [FA1FA1]    # Start drawing (new draw)
-    result += struct.pack("BBB", 0xFA, 0x30, 0x00 )   #   4003 0000   [FA3000]    # pen lift
-    result += struct.pack("BBB", 0x00, 0x00, 0x00 )   #   0000 0000   [000000]    # Move to X = 0, Y = 0
-    result += struct.pack("BBB", 0x3E, 0x83, 0xE8 )   #   1000 1000   [3E83E8]    # Move to X = 100mm, Y = 100mm
-    result += struct.pack("BBB", 0xFA, 0x40, 0x00 )   #   4004 0000   [FA4000]    # pen down (draw)
-    result += struct.pack("BBB", 0x5D, 0xC3, 0xE8 )   #   1500 1000   [5DC3E8]    # Move to  X = 150mm, Y = 100mm
-    result += struct.pack("BBB", 0xFA, 0x30, 0x00 )   #   4003 0000   [FA3000]    # Pen lift
-    result += struct.pack("BBB", 0x00, 0x00, 0x00 )   #   0000 0000   [000000]    # Move to 0,0
-    result += struct.pack("BBB", 0xFA, 0x20, 0x00 )   #   4002 0000   [FA2000]    # Stop drawing
-
-    return result
 
   def do_POST(self):
     # Jason :: TODO: I need to accept post requests for images and other 
@@ -223,7 +374,7 @@ def main():
   clientManager = ClientManager()
   try:
     handler = partial(MyHandler, clientManager)
-    server = HTTPServer(('', config.port), handler)
+    server = ThreadingHTTPServer(('', config.port), handler)
     logging.info('Starting httpserver...')
     server.serve_forever()
   except KeyboardInterrupt:
